3c27fc6e34248711044efdde2d1dbf7755a1c1ef
Myzwer <josh@onefortyfivedesign.com>
2022-01-15 17:32:59 -0500

    :construction: Updated part of the page to ACF.

--- frontpage.php
+++ frontpage.php
@@ -12,89 +12,150 @@
  */
 
 get_header(); ?>
-<div id="particles-js" id = "top"></div>
+<div id="particles-js" id="top"></div>
 <div class="header">
-    <img src="http://145-design.local/wp-content/uploads/2021/08/Header-blob.png" alt="">
+    <img src="<?php the_field('background_blob'); ?>" alt="">
     <div class="middle">
-        <h1>Hey!</h1>
-        <h1>I'm Josh.</h1>
-        <h2>I make awesome sites for awesome people.</h2>
+        <h1><?php the_field('title_1'); ?></h1>
+        <h1><?php the_field('title_2'); ?></h1>
+        <h2><?php the_field('subtitle'); ?></h2>
         <div class="mt-10 text-center">
-            <a class="bg-blue py-3 px-12 rounded-full text-black transition-colors duration-500" href="#contact">Contact Me</a>
+            <a class="bg-blue py-3 px-12 rounded-full text-black transition-colors duration-500"
+               href="<?php the_field('button_link'); ?>"><?php the_field('button_title'); ?></a>
         </div>
     </div>
 </div>
 
-<h1 class="text-center text-white opacity-60 text-6xl lg:text-8xl uppercase z-0 relative">About Me</h1>
+<h1 class="text-center text-white opacity-60 text-6xl lg:text-8xl uppercase z-0 relative"><?php the_field('section_title_1'); ?></h1>
 <div class="-mt-10 lg:-mt-12 sm:w-1/2 lg:w-1/4 text-center mx-auto relative">
-    <img class = "z-10" src="http://145-design.local/wp-content/uploads/2021/08/Headshot-Blob.png" alt="">
+    <img class="z-10" src="<?php the_field('headshot'); ?>" alt="">
 </div>
 
 <div class=" p-5 sm:w-1/2 text-center mx-auto relative">
-    <p class = "text-white py-2 text-left">Hey there! My name is Josh. I’m a full time web developer outside of Knoxville, TN. I work for Foothills Church as their web developer and I freelance on the side. I have around 4 years of experience in this field and love what I do.</p>
-    <p class = "text-white py-2  text-left">Creative people tend to be night owls, and I am no exception to that rule. 145 Design was started because the wee hours of the morning are not only when I make websites, but also my preferred time to be awake. </p>
-    <p class = "text-white py-2 text-left">In my free time you can find me playing computer games, board games or prepping for our next session of D&D. I also enjoy cheering on my favorite eSports team (cloud9), and listening to pop punk music because “the phase” never ended for me.</p>
-    <p class = "text-white py-2 text-left">I have been married to my sweet wife Katie for 4 years now, and we currently live just outside of Knoxville, TN with our 3 pets, Ahri, Xayah and Lux.</p>
+    <div class="text-white py-2 text-left">
+        <?php the_field('bio'); ?>
+    </div>
 </div>
 
-<img src="http://145-design.local/wp-content/uploads/2021/08/Services-Top-Compressed.png" alt="">
+<img src="<?php the_field('skill_top_image'); ?>" alt="">
 <div class="bg-purple text-white">
-    <h1 class="text-center opacity-60 text-6xl lg:text-8xl pt-5 uppercase">Skills</h1>
+    <h1 class="text-center opacity-60 text-6xl lg:text-8xl pt-5 uppercase"><?php the_field('section_title_2'); ?></h1>
     <div class="m-4 md:my-10 max-w-7xl text-center mx-auto">
         <div class="grid grid-cols-12 lg:-mt-14">
+
             <div class="col-span-12 md:col-span-4 p-5 text-center">
-                <h2 class = "massive opacity-20">UX</h2>
-                <h3 class = "text-2xl roboto uppercase -mt-40 lg:-mt-52">User Experience</h3>
-                <p class = " mt-20">“User Experience is a lot like a joke. If you have to explain it, it’s not that good.”</p>
+                <?php if (have_rows('skill_1')): ?>
+                    <?php while (have_rows('skill_1')): the_row(); ?>
+                        <h2 class="massive opacity-20">
+                            <?php the_sub_field('abbreviation'); ?>
+                        </h2>
+
+                        <h3 class="text-2xl roboto uppercase -mt-40 lg:-mt-52">
+                            <?php the_sub_field('title'); ?>
+                        </h3>
+
+                        <p class=" mt-20">
+                            <?php the_sub_field('subtitle'); ?>
+                        </p>
+
+                    <?php endwhile; ?>
+                <?php endif; ?>
             </div>
 
             <div class="col-span-12 md:col-span-4 p-5 text-center">
-                <h2 class = "massive opacity-20">UI</h2>
-                <h3 class = "text-2xl roboto uppercase -mt-40 lg:-mt-52">User Interface</h3>
-                <p class = " mt-20">“Everything is designed. Far less is designed well.”</p>
+                <?php if (have_rows('skill_2')): ?>
+                    <?php while (have_rows('skill_2')): the_row(); ?>
+                        <h2 class="massive opacity-20">
+                            <?php the_sub_field('abbreviation'); ?>
+                        </h2>
+
+                        <h3 class="text-2xl roboto uppercase -mt-40 lg:-mt-52">
+                            <?php the_sub_field('title'); ?>
+                        </h3>
+
+                        <p class=" mt-20">
+                            <?php the_sub_field('subtitle'); ?>
+                        </p>
+
+                    <?php endwhile; ?>
+                <?php endif; ?>
             </div>
 
 
             <div class="col-span-12 md:col-span-4 p-5 text-center">
-                <h2 class = "massive opacity-20">WD</h2>
-                <h3 class = "text-2xl roboto uppercase -mt-40 lg:-mt-52">Web Development</h3>
-                <p class = " mt-20">“My code better start doing what I want it to do instead of what I’m telling it to do.”</p>
+                <?php if (have_rows('skill_3')): ?>
+                    <?php while (have_rows('skill_3')): the_row(); ?>
+                        <h2 class="massive opacity-20">
+                            <?php the_sub_field('abbreviation'); ?>
+                        </h2>
+
+                        <h3 class="text-2xl roboto uppercase -mt-40 lg:-mt-52">
+                            <?php the_sub_field('title'); ?>
+                        </h3>
+
+                        <p class=" mt-20">
+                            <?php the_sub_field('subtitle'); ?>
+                        </p>
+
+                    <?php endwhile; ?>
+                <?php endif; ?>
             </div>
+
         </div>
     </div>
-    <img src="http://145-design.local/wp-content/uploads/2021/10/Projects-Top.png" alt="" class = "w-full">
+    <img src="http://145-design.local/wp-content/uploads/2021/10/Projects-Top.png" alt="" class="w-full">
 </div>
 
 <div class="bg-black-background text-white">
-    <h1 class="text-center opacity-60 text-6xl lg:text-8xl pt-5 uppercase mb-20">Projects</h1>
+    <h1 class="text-center opacity-60 text-6xl lg:text-8xl pt-5 uppercase mb-20"><?php the_field('section_title_3'); ?></h1>
     <div class="m-4 md:my-10 max-w-7xl text-center mx-auto">
-        <div class="grid grid-cols-12 mb-20">
-            <div class="col-span-12 md:col-span-5 p-5 text-center">
-                <img class = "drop-shadow-2xl" src="http://145-design.local/wp-content/uploads/2021/10/Busy-Pastor-Site.jpg" alt="">
-            </div>
-            <div class="col-span-12 md:col-span-7 p-5 text-left">
-                <h3><span class = "text-purple">Site:</span> The Busy Pastor</h3>
-                <h4><span class = "text-purple">URL:</span>  <a href="busypastor.org">busypastor.org</a></h4>
-                <h4><span class = "text-purple">Role:</span>  Designer and Developer</h4>
-                <h4><span class = "text-purple">Details:</span>  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque consectetur consequuntur dicta doloremque esse eum fugiat illum impedit, libero nemo nihil nisi quibusdam repellendus sed sint tempora veritatis voluptate, voluptates.</h4>
-                <h4><span class = "text-purple">Stack:</span> Wordpress</h4>
-                <div class="mt-10">
-                <a class="bg-purple py-3 px-12 rounded-full text-white transition-colors duration-500" href="#">View Live</a>
+
+
+        <?php
+        if (have_rows('project')):
+            while (have_rows('project')) : the_row(); ?>
+
+                <div class="grid grid-cols-12 mb-20">
+                    <div class="col-span-12 md:col-span-5 p-5 text-center">
+                        <img class="drop-shadow-2xl"
+                             src="<?php the_sub_field('site_image'); ?>" alt="">
+                    </div>
+                    <div class="col-span-12 md:col-span-7 p-5 text-left">
+                        <h3><span class="text-purple">Site:</span> <?php the_sub_field('site_title'); ?></h3>
+                        <h4><span class="text-purple">URL:</span> <a href="<?php the_sub_field('link'); ?>"><?php the_sub_field('link'); ?></a></h4>
+                        <h4><span class="text-purple">Role:</span> <?php the_sub_field('role'); ?></h4>
+                        <h4><span class="text-purple">Details:</span>
+                            <?php the_sub_field('details'); ?></h4>
+                        <h4><span class="text-purple">Stack:</span> <?php the_sub_field('stack'); ?></h4>
+                        <div class="mt-10">
+                            <a class="bg-purple py-3 px-12 rounded-full text-white transition-colors duration-500"
+                               href="<?php the_sub_field('link'); ?>">View
+                                Live</a>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            <?php
+            endwhile;
+        else :
+        endif; ?>
+
+
         <div class="grid grid-cols-12 mb-20">
             <div class="col-span-12 md:col-span-5 p-5 text-center">
-                <img class = "drop-shadow-2xl" src="http://145-design.local/wp-content/uploads/2021/10/Busy-Pastor-Site.jpg" alt="">
+                <img class="drop-shadow-2xl"
+                     src="http://145-design.local/wp-content/uploads/2021/10/Busy-Pastor-Site.jpg" alt="">
             </div>
             <div class="col-span-12 md:col-span-7 p-5 text-left">
-                <h3><span class = "text-purple">Site:</span> The Busy Pastor</h3>
-                <h4><span class = "text-purple">URL:</span>  <a href="busypastor.org">busypastor.org</a></h4>
-                <h4><span class = "text-purple">Role:</span>  Designer and Developer</h4>
-                <h4><span class = "text-purple">Details:</span>  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque consectetur consequuntur dicta doloremque esse eum fugiat illum impedit, libero nemo nihil nisi quibusdam repellendus sed sint tempora veritatis voluptate, voluptates.</h4>
-                <h4><span class = "text-purple">Stack:</span> Wordpress</h4>
+                <h3><span class="text-purple">Site:</span> The Busy Pastor</h3>
+                <h4><span class="text-purple">URL:</span> <a href="busypastor.org">busypastor.org</a></h4>
+                <h4><span class="text-purple">Role:</span> Designer and Developer</h4>
+                <h4><span class="text-purple">Details:</span> Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Atque consectetur consequuntur dicta doloremque esse eum fugiat illum impedit, libero nemo nihil
+                    nisi quibusdam repellendus sed sint tempora veritatis voluptate, voluptates.</h4>
+                <h4><span class="text-purple">Stack:</span> Wordpress</h4>
                 <div class="mt-10">
-                    <a class="bg-purple py-3 px-12 rounded-full text-white transition-colors duration-500" href="#">View Live</a>
+                    <a class="bg-purple py-3 px-12 rounded-full text-white transition-colors duration-500" href="#">View
+                        Live</a>
                 </div>
             </div>
         </div>
@@ -103,7 +164,7 @@ get_header(); ?>
 
 <img src="http://145-design.local/wp-content/uploads/2021/08/Services-Top-Compressed.png" alt="">
 <div class="bg-purple text-white">
-    <h1 id = "contact" class="text-center opacity-60 text-6xl lg:text-8xl pt-5 uppercase">Contact</h1>
+    <h1 id="contact" class="text-center opacity-60 text-6xl lg:text-8xl pt-5 uppercase">Contact</h1>
     <div class="m-4 md:my-10 max-w-7xl text-center mx-auto">
         <div class="grid grid-cols-12 lg:-mt-14">
             <?php if (have_posts()) : while (have_posts()) : the_post();
@@ -118,8 +179,8 @@ get_header(); ?>
 
 <div class="text-center text-white py-10">
     <p>Oh hey.</p>
-        <p>You made it to the footer.</p>
-        <p>Thanks for that. ❤️</p>
+    <p>You made it to the footer.</p>
+    <p>Thanks for that. ❤️</p>
     <a href="#top">^ Back to top ^</a>
 </div>
 
